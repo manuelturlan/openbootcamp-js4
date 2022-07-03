57412c02d4a1d5f699ef1adf180998c60693aef9
manuel <manuelturlan14@gmail.com>
2022-07-02 22:56:03 -0400

    Ejercicio 5 de javascript terminado

---
+++ index.js
@@ -0,0 +1,6 @@
+let myHeight = 176;
+let myHeight2 = 1.76;
+let myWeight = 62.3;
+let myHeightCeil = Math.ceil(myHeight);
+let myWeightFloor = Math.floor(myWeight);
+let max = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
